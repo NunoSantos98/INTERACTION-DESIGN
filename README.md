31dc90a582a99b54dab523901677e0e28f1e0647
NunoSantos98 <4goncn14@solent.ac.uk>
2020-12-21 01:38:12 +0000

    Week9

--- README.md
+++ README.md
@@ -23,7 +23,12 @@ https://webdevtrick.com/css-expand-button-on-hover/
 https://www.pluralsight.com/guides/how-to-get-selected-value-from-a-mapped-select-input-in-react
 
 
+---WEEK 8---
+https://theweek.com/articles/458134/why-should-actually-build-death-star
 
+---week9---
+
+https://v2.clayui.com/docs/css-framework/satellites/toggle-switch.html
 
 
 -->
--- src/Components/Week9/index.js
+++ src/Components/Week9/index.js
@@ -1,59 +1,121 @@
-import React from "react";
-import styled from "styled-components";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import TopNavigation from "../../Assets/Topnav.png"
-import loadingGIF from "../../Assets/loading.gif"
- 
+import React, { useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
+
+import day from "../../Assets/day.jpg";
+import night from "../../Assets/night.jpg";
+
+const time = '0.25s';
+
+
+
+const setAnimation = (from, to) => keyframes({ from: from, to: to })
+
+
+class Car extends React.Component {
+    constructor(props) {
+       super(props);
+
+       this.state = {
+        brand: "Ford",
+        model: "Mustang",
+        color: "red",
+        year: 1964
+      };
+}
+}
+
 const StyledWrapper = styled.div`
-width: 100vw;
-height: 100vh;
-display: flex;
-justify-content: center;
-align-items: center;
-
-`;
-const TopNav =styled.div`
-width: 360px;
-height: 80px;
-
+animation: ${time} ${({ showBackground }) => (showBackground && setAnimation({backgroundImage: 'url('+day+')',backgroundSize:"Cover",height: "100vh",transition: "0.2s all ease-out", color: '#000'}, {backgroundImage: 'url('+night+')',backgroundSize:"Cover",height: "100vh",transition: "0.2s all ease-out", color: '#000   '})) || (!showBackground && setAnimation({backgroundImage: 'url('+night+')',backgroundSize:"Cover",height: "100vh",transition: "0.2s all ease-out"}, {backgroundImage: 'url('+day+')',backgroundSize:"Cover",height: "100vh"}))} linear;
+animation-fill-mode: forwards;
+    display: block;
+    height: 100vh;
+    width: 100vw;
+    justify-content: center;
+    transition: 0.25s all ease-out;
+    align-items: center;
 `;
 
-const styles = styled.image`
+const StyledButton = styled.div`
+    animation: ${time} ${({ active }) => (active && setAnimation({backgroundColor: '#fafafa', color: '#333'}, {backgroundColor: '#fff', color: '#fff'})) || (!active && setAnimation({backgroundColor: '#333', color: '#fff'}, {backgroundColor: '#313130', color: '#333'}))} linear;
 
-width: 135px;
-height: 135px;
+    border-radius: 5rem;
+    border: solid 0.2rem;
+    background-color: transparent;
+    color: #fff;
+    height: 3rem;
+    width: 6rem;
+    float: right;
+    margin: 2rem;
+`;
+
+const StyledSwitch = styled.div`
+    border-radius: 5rem;    
+    animation: ${time} ${({ active }) => (active && setAnimation({marginLeft: 0}, {marginLeft: '50px'})) || (!active && setAnimation({marginLeft: '47px'}, {marginLeft: '7px'}))} linear;
+    animation-fill-mode: forwards;
+    border: solid 0.2rem;
+    height: 2rem;
+    width: 2rem;
+    margin: 0.3rem;
+    background: #fff;
+    
+     
 `;
 
 
-const StyledHeader =styled.h1`
-color: pink;
-margin-left:13rem;
-display:block;
-
-font-size: 300%;
 
 
+
+const ContentWrapper = styled.div`
+    width: 80%;
+    height: 25vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    margin-top: 2%;
+    margin-left: 10%;
+    animation: ${time} ${({ showBackground }) => (showBackground && setAnimation({color: '#333'}, {color: '#fff'})) || (!showBackground && setAnimation({color: '#fff'}, {color: '#333'}))} linear; 
+    animation-fill-mode: forwards;
 `;
 
 
+const Toggle = (props) => {
+    const [active, setActive] = useState(false);
+    const { onActive, onNotActive } = props;
+
+    useEffect(() => {
+        if (!active) {
+            onNotActive();
+            return;
+        }
+
+        onActive();
 
 
+    }, [active])
+
+
+    const handleClick = () => setActive(!active);
+
+    return (<StyledButton> <StyledSwitch active={active} onClick={handleClick} /> </StyledButton>)
+}
 
 
 const Week9 = () => {
+   
+    const [showBackground, setShowBackground] = useState(false);
+    const handleNotActive = () => setShowBackground(false);
+    const handleActive = () => setShowBackground(true);
+
+    
+    return (<StyledWrapper showBackground={showBackground}>
+        <Toggle onNotActive={handleNotActive} onActive={handleActive} />
 
 
-    return(
-
-        <StyledWrapper>
-        <StyledHeader>Page in development</StyledHeader>
-        <img src={loadingGIF} />
-
-        </StyledWrapper>
-    );
-
-
+    </StyledWrapper>)
 }
 
+
 export default Week9;
+
